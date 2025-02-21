39d4dc09084d4b2713321246b9a006d3d4927d02
KingsDev <63374258+KingsMMA@users.noreply.github.com>
2025-02-21 20:17:10 +1000

    Implemented config

---
+++ src/main/java/dev/kingrabbit/slashbed/Config.java
@@ -0,0 +1,53 @@
+package dev.kingrabbit.slashbed;
+
+import com.google.gson.Gson;
+import net.fabricmc.loader.api.FabricLoader;
+import org.apache.commons.io.IOUtils;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+public class Config {
+
+    public String teleportedMessage = "Teleported to bed spawn location.";
+
+    // region Saving and loading - Heavily inspired by https://github.com/Patbox/get-off-my-lawn-reserved/blob/1.21.4/src/main/java/draylar/goml/config/GOMLConfig.java#L102
+    // Credit to Patbox on GitHub, licensed under MIT
+    private static final Gson GSON = new Gson();
+
+    public static Config loadOrCreateConfig() {
+        try {
+            Config config;
+            File configFile = new File(FabricLoader.getInstance().getConfigDir().toFile(), "slashbed.json");
+
+            if (configFile.exists()) {
+                String json = IOUtils.toString(new InputStreamReader(new FileInputStream(configFile), StandardCharsets.UTF_8));
+
+                config = GSON.fromJson(json, Config.class);
+            } else {
+                config = new Config();
+            }
+
+            saveConfig(config);
+            return config;
+        } catch(IOException exception) {
+            SlashBed.LOGGER.error("Something went wrong while reading config!");
+            exception.printStackTrace();
+            return new Config();
+        }
+    }
+
+    public static void saveConfig(Config config) {
+        File configFile = new File(FabricLoader.getInstance().getConfigDir().toFile(), "slashbed.json");
+        try {
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(configFile), StandardCharsets.UTF_8));
+            writer.write(GSON.toJson(config));
+            writer.close();
+        } catch (Exception e) {
+            SlashBed.LOGGER.error("Something went wrong while saving config!");
+            e.printStackTrace();
+        }
+    }
+    // endregion
+
+}
--- src/main/java/dev/kingrabbit/slashbed/SlashBed.java
+++ src/main/java/dev/kingrabbit/slashbed/SlashBed.java
@@ -3,8 +3,10 @@ package dev.kingrabbit.slashbed;
 import net.fabricmc.api.ModInitializer;
 
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import net.minecraft.world.TeleportTarget;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,8 +14,10 @@ import org.slf4j.LoggerFactory;
 public class SlashBed implements ModInitializer {
 
 	public static final String MOD_ID = "slashbed";
-
+	public static final String VERSION = "1.0";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	public static Config CONFIG = new Config();
 
 	@Override
 	public void onInitialize() {
@@ -27,10 +31,21 @@ public class SlashBed implements ModInitializer {
 				}
 
 				context.getSource().getPlayer().teleportTo(context.getSource().getPlayer().getRespawnTarget(false, TeleportTarget.NO_OP));
-				context.getSource().sendFeedback(() -> Text.literal("Teleported to bed spawn location."), false);
+				context.getSource().sendFeedback(() -> Text.literal(CONFIG.teleportedMessage).formatted(Formatting.GREEN), false);
 				return 1;
 			}));
+
+			dispatcher.register(CommandManager.literal("slashbed").requires(source -> source.hasPermissionLevel(2)).executes(context -> {
+				context.getSource().sendFeedback(() -> Text.literal("Running SlashBed v" + VERSION + " (modid: " + MOD_ID + ")").formatted(Formatting.GREEN), false);
+				return 1;
+			}).then(CommandManager.literal("reload").executes(context -> {
+				CONFIG = Config.loadOrCreateConfig();
+				context.getSource().sendFeedback(() -> Text.literal("Reloaded SlashBed's config.").formatted(Formatting.GREEN), false);
+                return 1;
+            })));
 		});
+
+		ServerLifecycleEvents.SERVER_STARTING.register((s) -> CONFIG = Config.loadOrCreateConfig());
 	}
 
 }
