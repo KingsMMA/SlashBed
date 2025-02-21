fed49023b5c94129a771a8c5189f97a16d7a4289
KingsDev <63374258+KingsMMA@users.noreply.github.com>
2025-02-21 19:53:17 +1000

    Added /bed

--- src/main/java/dev/kingrabbit/slashbed/SlashBed.java
+++ src/main/java/dev/kingrabbit/slashbed/SlashBed.java
@@ -2,6 +2,10 @@ package dev.kingrabbit.slashbed;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.text.Text;
+import net.minecraft.world.TeleportTarget;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,6 +18,19 @@ public class SlashBed implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		LOGGER.info("SlashBed initialising");
+
+		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+			dispatcher.register(CommandManager.literal("bed").executes(context -> {
+				if (!context.getSource().isExecutedByPlayer()) {
+					context.getSource().sendError(Text.literal("This command can only be executed by a player."));
+					return 1;
+				}
+
+				context.getSource().getPlayer().teleportTo(context.getSource().getPlayer().getRespawnTarget(false, TeleportTarget.NO_OP));
+				context.getSource().sendFeedback(() -> Text.literal("Teleported to bed spawn location."), false);
+				return 1;
+			}));
+		});
 	}
 
 }
